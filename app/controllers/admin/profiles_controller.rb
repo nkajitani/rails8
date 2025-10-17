11e741f2c6d43415254bbe3ba45d06f7cb608cc7
梶谷 直幹 <kajitani719@gmail.com>
2025-10-18 07:56:13 +0900

    ユーザー編集画面作成

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -1,10 +1,28 @@
 class Admin::ProfilesController < Admin::BaseController
-  def show
-  end
+  def show; end
 
   def edit
+    set_minimum_password_length
   end
 
   def update
+    if current_user.update_without_current_password(account_update_params)
+      bypass_sign_in current_user
+      redirect_to admin_profile_path, notice: "プロフィールを更新しました。"
+    else
+      set_minimum_password_length
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Sets minimum password length to show to user
+  def set_minimum_password_length
+    @minimum_password_length = User.password_length.min
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 # ユーザー
 class User < ApplicationRecord
+  include Devise::Models::DatabaseAuthenticatable
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
+  def update_without_current_password(params)
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = update(params)
+
+    clean_up_passwords
+
+    result
+  end
 end
