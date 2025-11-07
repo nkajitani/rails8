ab4e820438b92ccee3c8ffec6e213f17b35b284d
nkajitani <kajitani719@gmail.com>
2025-11-07 23:53:09 +0900

    add models spec of comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'associates' do
+    is_expected.to belong_to(:post)
+  end
+
+  it 'validations' do
+    is_expected.to validate_presence_of(:body)
+    is_expected.to validate_presence_of(:name)
+  end
 end
