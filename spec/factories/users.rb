90a2e48a729e28bbbb95d6c7603db87160959d6b
nkajitani <kajitani719@gmail.com>
2025-11-07 09:07:42 +0900

    set factory_bot of models

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
+    sequence(:name) { |n| "User #{n}" }
     password { "password123" }
     password_confirmation { password }
   end
