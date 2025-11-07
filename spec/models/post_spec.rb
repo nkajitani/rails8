fba53097cca0021ca08ee825dc7ddbf83b5189b2
nkajitani <kajitani719@gmail.com>
2025-11-07 23:54:04 +0900

    add association case in models spec of post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it 'associates' do
+    is_expected.to have_many(:comments).dependent(:destroy)
+  end
+
   it 'validations' do
     is_expected.to validate_presence_of(:title)
     is_expected.to validate_presence_of(:body)
