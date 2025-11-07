449ba100afbf0658b823fb157c1cc0e35b9d8f29
nkajitani <kajitani719@gmail.com>
2025-11-07 13:12:18 +0900

    refactor User::RegistrationsController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -82,8 +82,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
-  def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
-  end
 end
