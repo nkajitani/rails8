9f305523cdbee0615350642d34a9f473da3b0fc1
nkajitani <kajitani719@gmail.com>
2025-11-07 09:38:44 +0900

    add specs for models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validations' do
+    is_expected.to validate_presence_of(:title)
+    is_expected.to validate_presence_of(:body)
+    is_expected.to validate_presence_of(:image)
+    is_expected.to validate_presence_of(:published_at)
+  end
 end
